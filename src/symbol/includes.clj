ac024f611b810d3bd07f9381e42050e53294e480
Timo Westkämper <timo.westkamper@mysema.com>
2012-11-09 00:20:39 +0200

    #19 added basic scanning of includes

---
+++ src/symbol/includes.clj
@@ -0,0 +1,58 @@
+(ns symbol.includes
+  (:import [java.io File])
+  (:use [clojure.data.zip.xml :only (attr attr= text xml-> xml1->)] 
+        [clojure.java.shell :only [sh]])
+  (:require [clojure.data.zip :as zf]
+            [clojure.xml :as xml]
+            [clojure.zip :as zip]))
+
+(def default-paths
+  ["/usr/local/include"
+   "/usr/include/c++/4.6.3"
+   "/usr/include"])
+
+(defn get-file
+  [search-paths local-path]
+  (loop [paths default-paths]
+    (let [f (File. (first paths) local-path)]
+      (cond (.exists f) f
+            (seq paths) (recur (rest paths))))))          
+
+(def get-xml (comp zip/xml-zip xml/parse))
+
+(defn- dump  
+  [local-path]
+  (if-let [f (get-file default-paths local-path)]
+    (slurp f)))
+          
+; TODO cache includes, maybe via memoize
+
+(defn include
+  [local-path]
+  (if-let [f (get-file default-paths local-path)]
+    (let [temp (doto (File/createTempFile "gccxml" "xml")
+                 (.deleteOnExit))
+          out  (sh "gccxml" (.getAbsolutePath f) (str "-fxml=" (.getAbsolutePath temp)))
+          xml  (get-xml temp)
+          ; TODO improve type handling, maybe via reduce or loop
+          fundamentals  (into {} (for [type (xml-> xml :FundamentalType)]
+                                   [(xml1-> type (attr :id)) (xml1-> type (attr :name))]))
+          cvqualified (into {} (for [type (xml-> xml :CvQualifiedType)]
+                                   [(xml1-> type (attr :id)) 
+                                    (fundamentals (xml1-> type (attr :type)))]))          
+          types (merge fundamentals cvqualified)
+          pointers (into {} (for [type (xml-> xml :PointerType)]
+                              [(xml1-> type (attr :id)) 
+                               (list 'pointer (types (xml1-> type (attr :type))))]))
+          types (merge types pointers)
+          functions (for [function (xml-> xml :Function)]
+                      (list (xml1-> function (attr :name))
+                            (map types (xml-> function :Argument (attr :type)))))]
+      (remove #(.startsWith (first %) "__") functions))))
+
+(defn include-pp
+  [local-path]
+  (doseq [function (include local-path)]
+    (println function)))
+          
+      
