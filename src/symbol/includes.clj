47b225b32f5ef9efcc77fbcd2757827dcf2e37f9
Timo Westkämper <timo.westkamper@mysema.com>
2012-11-09 00:21:33 +0200

    added copyright header

--- src/symbol/includes.clj
+++ src/symbol/includes.clj
@@ -1,3 +1,11 @@
+;   Copyright (c) Timo Westkämper. All rights reserved.
+;   The use and distribution terms for this software are covered by the
+;   Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+;   which can be found in the file epl-v10.html at the root of this distribution.
+;   By using this software in any fashion, you are agreeing to be bound by
+;   the terms of this license.
+;   You must not remove this notice, or any other, from this software.
+
 (ns symbol.includes
   (:import [java.io File])
   (:use [clojure.data.zip.xml :only (attr attr= text xml-> xml1->)] 
@@ -6,6 +14,7 @@
             [clojure.xml :as xml]
             [clojure.zip :as zip]))
 
+; TODO make this independent of the gcc version
 (def default-paths
   ["/usr/local/include"
    "/usr/include/c++/4.6.3"
