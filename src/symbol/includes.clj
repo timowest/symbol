52dfb3211066085b995c64f23d59f2a206f0a37d
Timo Westkämper <timo.westkamper@mysema.com>
2012-11-15 00:53:07 +0200

    #19 worked on includes

--- src/symbol/includes.clj
+++ src/symbol/includes.clj
@@ -55,42 +55,88 @@
     "long double" ldouble
     "unsigned long double" uldouble})
           
-; TODO cache includes, maybe via memoize
-
 (defn xml-get
-  [xml type k v]
+  [xml type k & vs]
   (into {} (for [entry (xml-> xml type)]
     [(xml1-> entry (attr k))
-     (xml1-> entry (attr v))])))
+     (assoc
+       (into {} (for [v vs]
+                  [v (xml1-> entry (attr v))]))
+       :cat type)])))
 
 (defn mapv
   [f m]
   (into {} (for [[k v] m] [k (f v)])))
-             
-; TODO ReferenceType, ArrayType, Struct
-; TODO normalize type names
+
+(declare typedef)
+
+; TODO defmulti
+(def typedefs
+  {:ArrayType (fn [all t] (list 'array (typedef all (:type t)) (:size t)))
+   :CvQualifiedType (fn [all t] (typedef all (:type t)))
+   :FundamentalType (fn [all t] (cpp-types (:name t)))
+   :PointerType (fn [all t] (list 'pointer (typedef all (:type t))))
+   :ReferenceType (fn [all t] (list 'reference (typedef all (:type t))))
+   :Typedef (fn [all t] (typedef all (:type t)))
+   :Union (fn [all t] (cons 'union
+                            (map #(typedef all %)
+                                 (.split (:members t) " "))))
+   :Constructor (fn [all t] nil) ;TODO
+   :Destructor (fn [all t] nil) ;TODO
+   :OperatorMethod (fn [all t] (list (:name t) 
+                                     (list 'fn [] (typedef all (:returns t)))))
+   :FunctionType (fn [all t] (list 'fn (typedef all (:returns t))))                               
+   :Field (fn [all t] (list (:name t) (typedef all (:type t))))
+   :Struct (fn [all t] (if (:members t) 
+                         (let [members (map #(typedef all %) 
+                                            (.split (:members t) " "))]
+                           (list 'struct (:name t) members))
+                         (list 'struct (:name t))))})
+                             
+(defn typedef
+  [types id]
+  (let [type (types id)
+        f (typedefs (:cat type))]
+    (if f
+      (f types type)
+      (throw (IllegalStateException. (str "No function for " id))))))
+   
+; TODO Destructor, OperatorMethod, Constructor
 (defn include
   [local-path]
   (if-let [f (get-file default-paths local-path)]
     (let [temp (doto (File/createTempFile "gccxml" "xml")
                  (.deleteOnExit))
           out  (sh "gccxml" (.getAbsolutePath f) (str "-fxml=" (.getAbsolutePath temp)))
-          xml  (get-xml temp)
-          ; TODO improve type handling, maybe via reduce or loop
-          fundamentals (->> (xml-get xml :FundamentalType :id :name)
-                         (mapv cpp-types))
-          cvqualified (->> (xml-get xml :CvQualifiedType :id :type)
-                        (mapv fundamentals))                    
-          types (merge fundamentals cvqualified)
-          pointers (->> (xml-get xml :PointerType :id :type)
-                    (mapv #(list 'pointer (types %))))          
-          types (merge types pointers)
+          xml  (get-xml temp)          
+          types (merge 
+                  (xml-get xml :ArrayType :id :type :size)
+                  (xml-get xml :CvQualifiedType :id :type)
+                  (xml-get xml :FundamentalType :id :name)                  
+                  (xml-get xml :PointerType :id :type)
+                  (xml-get xml :ReferenceType :id :type)
+                  (xml-get xml :Typedef :id :name :type)
+                  (xml-get xml :Union :id :members)
+                  (xml-get xml :Constructor :id :name) ; TODO args
+                  (xml-get xml :Destructor :id)
+                  (xml-get xml :OperatorMethod :id :name :returns) ; TODO args                  
+                  (xml-get xml :FunctionType :id :returns) ; TODO args
+                  (xml-get xml :Field :id :name :type)
+                  (xml-get xml :Struct :id :name :members))
+          typedefs (into {} (map (fn [id] [id (typedef types id)]) 
+                                 (keys types)))                            
+          variables (for [[id v] (xml-get xml :Variable :id :name :type)]
+                      (list (:name v) (typedefs (:type v))))                                    
           functions (for [function (xml-> xml :Function)]
                       (list (xml1-> function (attr :name))
                             (list 'fn
-                                  (map types (xml-> function :Argument (attr :type)))
-                                  (types (xml1-> function (attr :returns))))))]
-      (remove #(.startsWith (first %) "__") functions))))
+                                  (map typedefs (xml-> function :Argument (attr :type)))
+                                  (typedefs (xml1-> function (attr :returns))))))]
+      (concat
+        variables
+        (remove #(.startsWith (first %) "__") functions)))))
+
+;(def include (memoize include*))
 
 (defn include-pp
   [local-path]
