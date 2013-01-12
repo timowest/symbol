7aff5f212029d51a41cc453301294d12e94cc460
Timo Westkämper <timo.westkamper@mysema.com>
2013-01-12 13:07:07 +0200

    Update core.logic dependency

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "symbol"
   :dependencies [[org.clojure/clojure "1.4.0"]        
                  [org.clojure/clojure-contrib "1.2.0"]
-                 [org.clojure/core.logic "0.8.0-beta5"]
+                 [org.clojure/core.logic "0.8.0-rc1"]
                  [org.clojure/core.match "0.2.0-alpha11"]
                  [org.clojure/data.zip "0.1.1"]
                  [midje "1.4.0"]]
