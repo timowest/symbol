3b2fe4d415a44ccda3b43656f4cc7195402b038b
Timo Westkämper <timo.westkamper@mysema.com>
2013-02-06 21:55:47 +0200

    Extend pointer2 example

--- dev-resources/tests/pointers2.s
+++ dev-resources/tests/pointers2.s
@@ -1,4 +1,5 @@
-(ns pointers2)
+(ns pointers2
+  (include "functional"))
 
 ; XXX is array-of a good name, create might be too generic?
 (defmacro array-of [n expr]
@@ -16,11 +17,16 @@
 
 (deftype Envelope [^int type]) 
 
+(defn closure [arg1]
+  (fn [arg2]
+    (+ arg1 arg2 0.0)))
+
 (defn tests []
   (let [oscs (array-of 4 (Osc. 0i 0.0 0.0))
         lfos (array-of 4 (Lfo. 0i 0.0 0.0))
         filters (array-of 4 (Filter. 0i 0.0 0.0))
-        envs (array-of 4 (Envelope. 0i))]
+        envs (array-of 4 (Envelope. 0i))
+        fns (array-of 4 (closure 0.1))]
     0i))
   
   
